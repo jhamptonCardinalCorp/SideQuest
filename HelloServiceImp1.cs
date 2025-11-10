9866a3963eaa3c0ef6aaabf73e43be5873ebfb74
jhampton <jhampton@cardinalcorp.com>
2025-11-10 16:39:08 -0500

    Added gRPC service.

---
+++ HelloServiceImp1.cs
@@ -0,0 +1,13 @@
+﻿using Grpc.Core;
+
+namespace GrpcServicePlayground
+{
+    public class HelloServiceImpl : HelloService.HelloServiceBase
+    {
+        public override Task<HelloReply> SayHello(HelloRequest request, ServerCallContext context)
+        {
+            var reply = new HelloReply { Message = $"Hello, {request.Name}!" };
+            return Task.FromResult(reply);
+        }
+    }
+}
