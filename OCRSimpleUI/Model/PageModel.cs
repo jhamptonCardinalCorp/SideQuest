34edec379ed186441ba170b16ac50d338ce88ef4
jhh4e <79927564+jhh4e@users.noreply.github.com>
2025-12-31 04:54:08 -0600

    Started work on OCRSimpleUI.

---
+++ OCRSimpleUI/Model/PageModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OCRSimpleUI.Model
+{
+    public class PageModel
+    {
+        public int startCord {  get; set; } 
+        public int endCord { get; set; }
+    }
+}
